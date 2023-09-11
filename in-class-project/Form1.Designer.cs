989475a599c656efb1881b266ae19c8b4d77ac4e
Juan José Gómez <jjgomezswe@gmail.com>
2023-09-11 17:25:42 -0500

    Learned about how the designer works and the code behind it.

--- in-class-project/Form1.Designer.cs
+++ in-class-project/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.labelStockTicker = new System.Windows.Forms.Label();
             this.labelPeriod = new System.Windows.Forms.Label();
             this.comboBoxPeriod = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
             // 
             // buttonSearch
@@ -88,11 +89,21 @@
             this.comboBoxPeriod.Size = new System.Drawing.Size(121, 21);
             this.comboBoxPeriod.TabIndex = 8;
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(29, 142);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(577, 292);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.comboBoxPeriod);
             this.Controls.Add(this.labelPeriod);
             this.Controls.Add(this.labelStockTicker);
@@ -112,6 +123,7 @@
         private System.Windows.Forms.Label labelStockTicker;
         private System.Windows.Forms.Label labelPeriod;
         private System.Windows.Forms.ComboBox comboBoxPeriod;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
